81fc96cca65200ea3a5d3fed78d5325b4cf748ae
Priyam Harsh <priyamharsh14@gmail.com>
2020-09-02 15:20:46 +0530

    Update settings.py

--- cbwebsite/cbwebsite/settings.py
+++ cbwebsite/cbwebsite/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')2lqllxt(70tkf0g02&3+es_@p4)ks-t!@%dx0vh&f8sqr6ls&'
+# Put your own secret key here
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
